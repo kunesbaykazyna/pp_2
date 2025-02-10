fd810bce6f48e97cae23aa739387254430b25a0d
kunesbay kazyna <k_kunesbay@kbtu.kz>
2025-02-10 19:23:13 +0500

    lab w 4

--- lab_3/function-1/5.py
+++ lab_3/function-1/5.py
@@ -1,9 +1,9 @@
-def permutations(some):
-    n = len(some)
+def permutations(k):
+    n = len(k)
 
     for i in range(n):
         for j in range(n):
-            print(some[(j-i)], end=" ")
+            print(k[(j-i)], end=" ")
         print()
 k=str(input("soz:"))
 permutations(k)
